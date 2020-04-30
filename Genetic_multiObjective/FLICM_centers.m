7e11e7c4bf2fd59a22be3b451e355282048eac04
khaled-farouk <54776528+khaled-farouk@users.noreply.github.com>
2020-04-30 18:25:52 +0200

    Add files via upload

---
+++ Genetic_multiObjective/FLICM_centers.m
@@ -0,0 +1,14 @@
+
+function c = FLICM_centers(data,U,cnum,m)
+
+Len = length(data);
+c = zeros(1,cnum);
+
+for k=1:cnum
+    sSum=0;
+    for i=1:Len
+        sSum = sSum + U(i,k)^m;
+        c(k) = c(k) + (U(i,k)^m)*data(i);
+    end
+    c(k) = c(k)/sSum;
+end
---
+++ Genetic_multiObjective/channel_tf.m
@@ -0,0 +1,11 @@
+
+
+function [Hqq,Hrq] = channel_tf(Q,Nc)
+
+for i=1:Q
+    for j=1:Nc
+        Hqq(i,j) = 0.5*exp(-0.005*i*j);
+        Hrq(i,j) = 0.1*exp(-0.001*i*j);
+    end
+end
+
